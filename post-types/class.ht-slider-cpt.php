d01b0ff39427b37c8e1fd0ab3aaff524e37fbe26
Eddie622 <eddietorres622@gmail.com>
2023-05-31 21:51:11 -0400

    added metaboxes

--- post-types/class.ht-slider-cpt.php
+++ post-types/class.ht-slider-cpt.php
@@ -4,6 +4,11 @@ if( !class_exists( 'HT_Slider_Post_Type' ) ){
     class HT_Slider_Post_Type{
         function __construct(){
             add_action( 'init', array( $this, 'create_post_type' ) );
+            add_action( 'add_meta_boxes', array( $this, 'add_meta_boxes' ) );
+            add_action( 'save_post', array( $this, 'save_meta_boxes' ) );
+            add_filter( 'manage_ht-slider_posts_columns', array( $this, 'ht_slider_cpt_columns' ) );
+            add_action( 'manage_ht-slider_posts_custom_column', array( $this, 'ht_slider_custom_columns'), 10, 2 );
+            add_filter( 'manage_edit-ht-slider_sortable_columns', array( $this, 'ht_slider_sortable_columns' ) );
         }
 
         public function create_post_type(){
@@ -51,9 +56,81 @@ if( !class_exists( 'HT_Slider_Post_Type' ) ){
                     'exclude_from_search' => false,
                     'publicly_queryable' => true,
                     'show_in_rest' => true,
-                    'menu_icon' => 'dashicons-images-alt2'
+                    'menu_icon' => 'dashicons-images-alt2',
                 )
             );
+        }
+
+        public function ht_slider_cpt_columns( $columns ){
+            $columns['ht_slider_link_text'] = esc_html__( 'Link Text', 'ht-slider' );
+            $columns['ht_slider_link_url'] = esc_html__( 'Link URL', 'ht-slider' );
+            return $columns;
+        }
+
+        public function ht_slider_custom_columns( $column, $post_id ){
+            switch( $column ){
+                case 'ht_slider_link_text':
+                    echo esc_html( get_post_meta( $post_id, 'ht_slider_link_text', true ) );
+                break;
+                case 'ht_slider_link_url':
+                    echo esc_url( get_post_meta( $post_id, 'ht_slider_link_url', true ) );
+                break;                
+            }
+        }
+
+        public function ht_slider_sortable_columns( $columns ){
+            $columns['ht_slider_link_text'] = 'ht_slider_link_text';
+            return $columns;
+        }
+
+        public function add_meta_boxes(){
+            add_meta_box(
+                'ht_slider_meta_box',
+                __( 'Link options', 'ht-slider' ),
+                array( $this, 'add_inner_meta_boxes' ),
+                'ht-slider',
+                'normal',
+                'high'
+            );
+        }
+
+        public function add_inner_meta_boxes( $post ) {
+            require_once( HT_SLIDER_PATH . 'views/ht-slider_metabox.php' );
+        }
+
+        public function save_meta_boxes( $post_id ){
+            if( isset( $_POST['ht_slider_nonce'] ) && !wp_verify_nonce( $_POST['ht_slider_nonce'], 'ht_slider_nonce' ) ){
+                return;
+            }
+
+            if( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ){
+                return;
+            }
+
+            if( isset( $_POST['post_type'] ) && $_POST['post_type'] === 'ht-slider' ){
+                if( ! current_user_can( 'edit_page', $post_id ) || ! current_user_can( 'edit_post', $post_id ) ){
+                    return;
+                }
+            }
+
+            if( isset( $_POST['action'] ) && $_POST['action'] == 'editpost' ){
+                $old_link_text = get_post_meta( $post_id, 'ht_slider_link_text', true );
+                $new_link_text = $_POST['ht_slider_link_text'];
+                $old_link_url = get_post_meta( $post_id, 'ht_slider_link_url', true );
+                $new_link_url = $_POST['ht_slider_link_url'];
+
+                if( empty( $new_link_text )){
+                    update_post_meta( $post_id, 'ht_slider_link_text', 'Add some text' );
+                } else {
+                    update_post_meta( $post_id, 'ht_slider_link_text', sanitize_text_field( $new_link_text ), $old_link_text );
+                }
+
+                if( empty( $new_link_url )){
+                    update_post_meta( $post_id, 'ht_slider_link_url', '#' );
+                } else {
+                    update_post_meta( $post_id, 'ht_slider_link_url', sanitize_text_field( $new_link_url ), $old_link_url );
+                }
+            }
         }
     }
 }
---
+++ views/ht-slider_metabox.php
@@ -0,0 +1,37 @@
+<?php 
+    $link_text = get_post_meta( $post->ID, 'ht_slider_link_text', true );
+    $link_url = get_post_meta( $post->ID, 'ht_slider_link_url', true );
+?>
+<table class="form-table ht-slider-metabox"> 
+<input type="hidden" name="ht_slider_nonce" value="<?php echo wp_create_nonce( "ht_slider_nonce" ); ?>">
+    <tr>
+        <th>
+            <label for="ht_slider_link_text">Link Text</label>
+        </th>
+        <td>
+            <input 
+                type="text" 
+                name="ht_slider_link_text" 
+                id="ht_slider_link_text" 
+                class="regular-text link-text"
+                value="<?php echo ( isset( $link_text ) ) ? esc_html( $link_text ) : ''; ?>"
+                required
+            >
+        </td>
+    </tr>
+    <tr>
+        <th>
+            <label for="ht_slider_link_url">Link URL</label>
+        </th>
+        <td>
+            <input 
+                type="url" 
+                name="ht_slider_link_url" 
+                id="ht_slider_link_url" 
+                class="regular-text link-url"
+                value="<?php echo ( isset( $link_url ) ) ? esc_url( $link_url ) : ''; ?>"
+                required
+            >
+        </td>
+    </tr>               
+</table>
