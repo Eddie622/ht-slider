c4b647c8bfe9c0d438ccbbfe743b104708d6e2a3
Eddie622 <eddietorres622@gmail.com>
2023-06-09 00:45:41 -0400

    implemented paged functionality

--- views/ht-slider_settings.php
+++ views/ht-slider_settings.php
@@ -1,10 +1,22 @@
 <div class="wrap">
     <h1><?php echo esc_html__( get_admin_page_title() ); ?></h1>
+    <?php $active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'main_options'; ?>
+    <h2 class="nav-tab-wrapper">
+        <a href="?page=ht_slider_admin&tab=main_options" class="nav-tab" <?php echo esc_attr__( $active_tab == 'main_options' ? 'nav-tab-active' : ''); ?>>
+            <?php echo esc_html_e( 'Main Options', 'ht-slider' )?>
+        </a>
+        <a href="?page=ht_slider_admin&tab=additional_options" class="nav-tab" <?php echo esc_attr__( $active_tab == 'additional_options' ? 'nav-tab-active' : ''); ?>>
+            <?php echo esc_html_e( 'Additional Options', 'ht-slider' )?>
+        </a>
+    </h2>
     <form action="options.php" method="post">
-        <?php 
+        <?php
             settings_fields( 'ht_slider_group' );
-            do_settings_sections( 'ht_slider_page1' );
-            do_settings_sections( 'ht_slider_page2' );
+            if( $active_tab == 'main_options' ) {
+                do_settings_sections( 'ht_slider_page1' );
+            } else {
+                do_settings_sections( 'ht_slider_page2' );
+            }
             submit_button( 'Save Settings' );
         ?>
     </form>
