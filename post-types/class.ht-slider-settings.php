df0b3c5fe7941209ca4bee16a54c83b797dcf23a
Eddie622 <eddietorres622@gmail.com>
2023-06-08 21:11:49 -0400

    additional options added

--- post-types/class.ht-slider-settings.php
+++ post-types/class.ht-slider-settings.php
@@ -43,7 +43,10 @@ if( !class_exists( 'HT_Slider_Settings' ) ) {
                 'Slider Title',
                 array( $this, 'ht_slider_title_callback' ),
                 'ht_slider_page2',
-                'ht_slider_second_section'
+                'ht_slider_second_section',
+                array(
+                    'label_for' => 'ht_slider_title'
+                )
             );
 
             add_settings_field(
@@ -51,7 +54,10 @@ if( !class_exists( 'HT_Slider_Settings' ) ) {
                 'Slider Bullets',
                 array( $this, 'ht_slider_bullets_callback' ),
                 'ht_slider_page2',
-                'ht_slider_second_section'
+                'ht_slider_second_section',
+                array(
+                    'label_for' => 'ht_slider_bullets'
+                )
             );
 
             add_settings_field(
@@ -59,7 +65,14 @@ if( !class_exists( 'HT_Slider_Settings' ) ) {
                 'Slider Style',
                 array( $this, 'ht_slider_style_callback' ),
                 'ht_slider_page2',
-                'ht_slider_second_section'
+                'ht_slider_second_section',
+                array(
+                    'items' => array(
+                        'style-1',
+                        'style-2',
+                    ),
+                    'label_for' => 'ht_slider_style'
+                )
             );
         }
 
@@ -67,31 +80,29 @@ if( !class_exists( 'HT_Slider_Settings' ) ) {
             echo _e( '<span>Use the shortcode [ht_slider] to display the slider in any post/page/widget</span>' );
         }
 
-        public function ht_slider_title_callback() {
+        public function ht_slider_title_callback( $args ) {
             printf(
                 '<input type="text" id="ht_slider_title" name="ht_slider_options[ht_slider_title]" value="%s" />',
                 isset( self::$options['ht_slider_title'] ) ? esc_attr( self::$options['ht_slider_title'] ) : ''
             );
         }
 
-        public function ht_slider_bullets_callback() {
+        public function ht_slider_bullets_callback( $args ) {
             printf(
                 '<input type="checkbox" id="ht_slider_bullets" name="ht_slider_options[ht_slider_bullets]" value="1" %s />',
                 isset( self::$options['ht_slider_bullets'] ) ? 'checked' : ''
             );
         }
 
-        public function ht_slider_style_callback() {
-            printf(
-                '<select id="ht_slider_style" name="ht_slider_options[ht_slider_style]">
-                    <option value="style1" %s>Style 1</option>
-                    <option value="style2" %s>Style 2</option>
-                    <option value="style3" %s>Style 3</option>
-                </select>',
-                isset( self::$options['ht_slider_style'] ) && self::$options['ht_slider_style'] == 'style1' ? 'selected' : '',
-                isset( self::$options['ht_slider_style'] ) && self::$options['ht_slider_style'] == 'style2' ? 'selected' : '',
-                isset( self::$options['ht_slider_style'] ) && self::$options['ht_slider_style'] == 'style3' ? 'selected' : ''
-            );
+        public function ht_slider_style_callback( $args ) {
+            printf( '<select id="ht_slider_style" name="ht_slider_options[ht_slider_style]">' );
+            foreach( $args['items'] as $item ) {
+                printf(
+                    '<option value="%s" %s>%s</option>',
+                    esc_attr__( $item ), isset( self::$options['ht_slider_style'] ) && self::$options['ht_slider_style'] === $item ? 'selected' : '', esc_html__( ucfirst( $item ) )
+                );
+            }
+            printf( '</select>' );
         }
 
         public function ht_slider_options_validate( $input ) {
