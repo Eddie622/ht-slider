e9c11b2a11b903bb470606aa62a6d8dc514c8912
Eddie622 <eddietorres622@gmail.com>
2023-06-10 19:01:10 -0400

    updated output for consistency

--- class.ht-slider-settings.php
+++ class.ht-slider-settings.php
@@ -77,7 +77,7 @@ if( !class_exists( 'HT_Slider_Settings' ) ) {
         }
 
         public function ht_slider_shortcode_callback() {
-            echo ('<span>' . esc_html_e( 'Use the shortcode [ht_slider] to display the slider in any post/page/widget', 'ht-slider' ) . '</span>' );
+            printf('<span>%s</span>', esc_html_e( 'Use the shortcode [ht_slider] to display the slider in any post/page/widget', 'ht-slider' ) );
         }
 
         public function ht_slider_title_callback( $args ) {
