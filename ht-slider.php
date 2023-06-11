0e381157ac507a60f9afd8323ab594c6e14adbee
Eddie622 <eddietorres622@gmail.com>
2023-06-10 22:35:48 -0400

    implemented uninstall hook

--- ht-slider.php
+++ ht-slider.php
@@ -80,7 +80,19 @@ if ( !class_exists( 'HT_Slider' ) ){
         }
 
         public static function uninstall(){
+            delete_option( 'ht_slider_options' );
 
+            $posts = get_posts( 
+                array(
+                    'post_type' => 'ht-slider',
+                    'numberposts' => -1,
+                    'post_status' => 'any'
+                ) 
+            );
+
+            foreach( $posts as $post ){
+                wp_delete_post( $post->ID, true );
+            }
         }
 
         public function load_textdomain(){
